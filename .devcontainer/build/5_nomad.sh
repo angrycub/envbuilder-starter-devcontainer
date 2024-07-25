fcc87ff9a2b1f75cad2db99757b3f883380462b7
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2024-07-25 12:07:14 -0400

    Add DBUS to Dockerfile

---
+++ .devcontainer/build/5_nomad.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+log() {
+  echo "==> $(date) - ${1}"
+}
+log "Running ${0}"
+
+raw_exec_nomad()
+{
+echo '
+plugin "raw_exec" {
+  config {
+    enabled = true
+  }
+}
+' | sudo tee /etc/nomad.d/raw_exec.hcl
+}
+
+mkdir -p /etc/nomad.d
+raw_exec_nomad
